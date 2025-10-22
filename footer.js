973fc714100e370c25c81c21cbab1561257c507d
adrian-river <101651015+adrian-river@users.noreply.github.com>
2025-10-21 19:51:10 -0500

    Update footer.js | Arreglo del footer

--- footer.js
+++ footer.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const footerContainer = document.getElementById("footer");
 
-  const inPagesFolder = window.location.pathname.includes("/pages/");
-  const footerPath = inPagesFolder ? "footer.html" : "pages/footer.html";
+  const inPagesFolder = window.location.pathname.includes("./pages");
+  const footerPath = inPagesFolder ? "footer.html" : "./pages/footer.html";
 
   fetch(footerPath)
     .then(response => {
